b7ce630ef871080d07a0b26175b4aa5810f17ff4
mohamedreda1258 <mohamed reda.com>
2023-05-19 15:56:01 +0300

    first commit

---
+++ asstes/test.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector(' .container-fluid')
+
+function phones(phone) {
+    img.src = phone
+}
+
+function colors(color) {
+    container.style.background = color;
+}
